e8664ec38234c7397ffbcd3dfbec4a60c91d0ba0
Esther White <monacodelisa@gmail.com>
2024-06-08 20:13:56 +0300

    styling colors adjustments

--- src/app/components/join/join.component.ts
+++ src/app/components/join/join.component.ts
@@ -1,12 +1,104 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { FormBuilder, FormControl, FormsModule, NgForm, ReactiveFormsModule, Validators } from "@angular/forms";
+
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import {
+	MAT_RADIO_DEFAULT_OPTIONS,
+	MatRadioModule,
+} from "@angular/material/radio";
+import { MatFormField } from "@angular/material/form-field";
+import {MatStepperModule} from '@angular/material/stepper'
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-join',
   standalone: true,
-  imports: [],
+  imports: [
+		FormsModule,
+		ReactiveFormsModule,
+		RouterLink,
+		MatCardModule,
+		MatRadioModule,
+    MatFormField,
+    MatStepperModule,
+    MatInputModule,
+		MatButtonModule,
+    MatCheckboxModule,
+		MatIconModule,
+	],
+  providers: [
+		{
+			provide: MAT_RADIO_DEFAULT_OPTIONS,
+			useValue: { color: "primary" },
+		},
+	],
   templateUrl: './join.component.html',
   styleUrl: './join.component.scss'
 })
-export class JoinComponent {
+export class JoinComponent implements OnInit {
+  selection: string = "";
+  paidlancerJoin: boolean = false;
+  clientJoin: boolean = false;
+  selectionMade: boolean = false;
+  private _formBuilder = inject(FormBuilder)
+  email = new FormControl('', [Validators.required, Validators.email]);
+  acceptTOS = new FormControl(false, Validators.requiredTrue);
 
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  ngOnInit(): void {
+    this.paidlancerJoin = false;
+    this.clientJoin = false;
+    this.selectionMade = false;
+  }
+
+  onTalentJoin() {
+    this.paidlancerJoin = true;
+    this.selectionMade = true;
+    this.clientJoin = false;
+  }
+
+  onClientJoin() {
+    this.paidlancerJoin = false;
+    this.selectionMade = true;
+    this.clientJoin = true;
+  }
+
+  clearSelection() {
+    this.paidlancerJoin = false;
+    this.clientJoin = false;
+    this.selectionMade = false;
+  }
+
+  submitForm(form: NgForm) {
+    if (form.valid) {
+      // Form is valid, proceed with form submission
+      console.log("Form submitted successfully!");
+    }
+  }
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getTOSErrorMessage() {
+    if (this.acceptTOS.hasError('required')) {
+      return 'You must accept the Terms of Service.';
+    }
+
+    return '';
+  }
 }
