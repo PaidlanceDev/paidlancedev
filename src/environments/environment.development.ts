70843f2ea5df6271330c08a998f7262c96cde86b
Esther White <monacodelisa@gmail.com>
2024-06-10 12:38:50 +0300

    gettin a bit annoyed

--- src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  appname: 'Paidlancer',
-  supabase: {
-    url: process.env['NG_APP_SUPABASE_URL'] || "",
-    key: process.env['NG_APP_SUPABASE_API_KEY'] || ""
-  }
-};
--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  appname: 'Paidlancer',
-  supabase: {
-    url: "https://pytboioympljfyxuapia.supabase.co",
-    key: process.env['NG_APP_SUPABASE_API_KEY'] || ""
-  }
-};
