d58dd234fe23d1e9a1ae98d742c53018cbc30545
Esther White <monacodelisa@gmail.com>
2024-06-29 19:54:22 +0300

    part of setting up

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { RouterLink } from '@angular/router';
 import { TestService } from '../../../services/test.service';
+import { User } from '@supabase/supabase-js';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +15,19 @@ import { TestService } from '../../../services/test.service';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
+  user: User | null = null;
+  private authService = inject(AuthService);
   testService = inject(TestService);
 
   ngOnInit(): void {
+    this.authService.currentUser.subscribe((user) => {
+      if (user) {
+        this.user = user;
+        console.log("User:", this.user);
+      } else {
+        this.user = null;
+      }
+    });
     this.testService.getAllTestData();
   }
 }
