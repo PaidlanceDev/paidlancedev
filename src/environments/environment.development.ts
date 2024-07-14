8d0794d9b0c119686e0b397462a5f8a309d68787
Esther White <monacodelisa@gmail.com>
2024-07-14 11:08:40 +0300

    new og:image moving db

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   appname: 'PaidlanceDev',
   supabase: {
-    url: 'https://rerozlyheuykauquinbz.supabase.co',
+    url: 'https://tsnxveeuefcfimyzbvlm.supabase.co',
     key: 'NG_APP_SUPABASE_API_KEY'
   }
 };
